8ea7d1605c70516fe745fa354348f4a5b988f832
definev <daiduong.workmail@gmail.com>
2022-12-08 08:58:31 +0700

    separate gateway code, refactor

--- go-gateway/customer_loyalty.go
+++ go-gateway/customer_loyalty.go
@@ -10,29 +10,21 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path"
 	"time"
 
+	"github.com/definev/customer_loyalty_hyperledger/go-gateway/fabric"
 	"github.com/definev/customer_loyalty_hyperledger/go-gateway/handler"
 	"github.com/definev/customer_loyalty_hyperledger/go-gateway/middleware"
 	"github.com/gin-gonic/gin"
 	"github.com/hyperledger/fabric-gateway/pkg/client"
-	"github.com/hyperledger/fabric-gateway/pkg/identity"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 const (
-	mspID        = "Org1MSP"
-	cryptoPath   = "../test-network/organizations/peerOrganizations/org1.example.com"
-	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/cert.pem"
-	keyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore/"
-	tlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
-	peerEndpoint = "localhost:7051"
-	gatewayPeer  = "peer0.org1.example.com"
+	// TODO: Need to expose in environment variable
+	secret = "secret"
 )
-
-
 
 func main() {
 	log.Println("============ application-golang starts ============")
@@ -41,8 +33,8 @@ func main() {
 	clientConnection := newGrpcConnection()
 	defer clientConnection.Close()
 
-	id := newIdentity()
-	sign := newSign()
+	id := fabric.NewIdentity()
+	sign := fabric.NewSign()
 
 	// Create a Gateway connection for a specific client identity
 	gw, err := client.Connect(
@@ -75,7 +67,7 @@ func main() {
 	contract := network.GetContract(chaincodeName)
 
 	r := gin.Default()
-	r.Use(CORSMiddleware())
+	r.Use(middleware.CORSMiddleware())
 	api := r.Group("/api")
 
 	api.POST("/registerMember", handler.CreateMember(contract))
@@ -85,92 +77,27 @@ func main() {
 	api.POST("/memberData", handler.MemberData(contract))
 	api.POST("/partnerData", handler.PartnerData(contract))
 
-	api.Use(middleware.Auth("secret")).GET("/usePoints", handler.GetUsedPoints(contract))
-	api.Use(middleware.Auth("secret")).GET("/earnPoints", handler.GetEarnedPoints(contract))
+	api.Use(middleware.Auth(secret)).GET("/usePoints", handler.GetUsedPoints(contract))
+	api.Use(middleware.Auth(secret)).GET("/earnPoints", handler.GetEarnedPoints(contract))
 
 	r.Run(":8181")
 }
 
-
 // newGrpcConnection creates a gRPC connection to the Gateway server.
 func newGrpcConnection() *grpc.ClientConn {
-	certificate, err := loadCertificate(tlsCertPath)
+	certificate, err := fabric.LoadCertificate(fabric.TlsCertPath)
 	if err != nil {
 		panic(err)
 	}
 
 	certPool := x509.NewCertPool()
 	certPool.AddCert(certificate)
-	transportCredentials := credentials.NewClientTLSFromCert(certPool, gatewayPeer)
+	transportCredentials := credentials.NewClientTLSFromCert(certPool, fabric.GatewayPeer)
 
-	connection, err := grpc.Dial(peerEndpoint, grpc.WithTransportCredentials(transportCredentials))
+	connection, err := grpc.Dial(fabric.PeerEndpoint, grpc.WithTransportCredentials(transportCredentials))
 	if err != nil {
 		panic(fmt.Errorf("failed to create gRPC connection: %w", err))
 	}
 
 	return connection
-}
-
-// newIdentity creates a client identity for this Gateway connection using an X.509 certificate.
-func newIdentity() *identity.X509Identity {
-	certificate, err := loadCertificate(certPath)
-	if err != nil {
-		panic(err)
-	}
-
-	id, err := identity.NewX509Identity(mspID, certificate)
-	if err != nil {
-		panic(err)
-	}
-
-	return id
-}
-
-func loadCertificate(filename string) (*x509.Certificate, error) {
-	certificatePEM, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read certificate file: %w", err)
-	}
-	return identity.CertificateFromPEM(certificatePEM)
-}
-
-// newSign creates a function that generates a digital signature from a message digest using a private key.
-func newSign() identity.Sign {
-	files, err := os.ReadDir(keyPath)
-	if err != nil {
-		panic(fmt.Errorf("failed to read private key directory: %w", err))
-	}
-	privateKeyPEM, err := os.ReadFile(path.Join(keyPath, files[0].Name()))
-
-	if err != nil {
-		panic(fmt.Errorf("failed to read private key file: %w", err))
-	}
-
-	privateKey, err := identity.PrivateKeyFromPEM(privateKeyPEM)
-	if err != nil {
-		panic(err)
-	}
-
-	sign, err := identity.NewPrivateKeySign(privateKey)
-	if err != nil {
-		panic(err)
-	}
-
-	return sign
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
-
-        c.Next()
-    }
 }
---
+++ go-gateway/fabric/initialize.go
@@ -0,0 +1,68 @@
+package fabric
+
+import (
+	"crypto/x509"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/hyperledger/fabric-gateway/pkg/identity"
+)
+
+const (
+	MspID        = "Org1MSP"
+	cryptoPath   = "../test-network/organizations/peerOrganizations/org1.example.com"
+	CertPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/cert.pem"
+	KeyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore/"
+	TlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
+	PeerEndpoint = "localhost:7051"
+	GatewayPeer  = "peer0.org1.example.com"
+)
+
+// newIdentity creates a client identity for this Gateway connection using an X.509 certificate.
+func NewIdentity() *identity.X509Identity {
+	certificate, err := LoadCertificate(CertPath)
+	if err != nil {
+		panic(err)
+	}
+
+	id, err := identity.NewX509Identity(MspID, certificate)
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+func LoadCertificate(filename string) (*x509.Certificate, error) {
+	certificatePEM, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read certificate file: %w", err)
+	}
+	return identity.CertificateFromPEM(certificatePEM)
+}
+
+// newSign creates a function that generates a digital signature from a message digest using a private key.
+func NewSign() identity.Sign {
+	files, err := os.ReadDir(KeyPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key directory: %w", err))
+	}
+	privateKeyPEM, err := os.ReadFile(path.Join(KeyPath, files[0].Name()))
+
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key file: %w", err))
+	}
+
+	privateKey, err := identity.PrivateKeyFromPEM(privateKeyPEM)
+	if err != nil {
+		panic(err)
+	}
+
+	sign, err := identity.NewPrivateKeySign(privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return sign
+}
